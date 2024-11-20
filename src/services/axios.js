2bfecc1f34b20471615e9f5d2e7d0c444961687b
Peer Mohammad <peeru548@gmail.com>
2024-11-20 11:12:48 +0530

    fix : Ready to deploy

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const Api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true, 
-  timeout: 15000, 
 });
 
 Api.interceptors.response.use(
